04d0adb9d0cfe704c11be443a83840b755ca2c57
Hyzinx <hyzin633@gmail.com>
2026-03-06 08:25:37 +0200

    Add Get all movie play now in movies repository

--- lib/data/repositories/movie_repository.dart
+++ lib/data/repositories/movie_repository.dart
@@ -1,1 +1,15 @@
-class MovieRepository {}
+import 'package:hetro_anime/consts/strings.dart';
+import 'package:hetro_anime/data/api/movie_api.dart';
+import 'package:hetro_anime/data/models/movie.dart';
+import 'package:hetro_anime/data/models/response_result.dart';
+
+class MovieRepository {
+  final MovieApi movieApi;
+  MovieRepository(this.movieApi);
+  Future<List<Movie>> getAllMoviesPlayNow() async {
+    final ResponseResult response = await movieApi.getMoviesPlayNow(
+      "Bearer $apiKey",
+    );
+    return response.results!.map((movie) => Movie.fromJson(movie)).toList();
+  }
+}
